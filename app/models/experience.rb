da134ce30d47d74b033c18edffb7dd49ecdb09f4
Frank Ashurst <airongstring@gmail.com>
2014-03-20 04:09:08 -0300

    Adds sensible defaults to the Experience model.

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,9 +1,16 @@
 class Experience < ActiveRecord::Base
   validates :body, uniqueness: true, presence: true
-  
+
+  before_save :defaults
+
   default_scope {order('created_at DESC')}
 
   def self.random
     order('RANDOM()').first
+  end
+
+  def defaults
+    self.pseudonym ||= 'anonymous'
+    self.title ||= 'untitled experience'
   end
 end
