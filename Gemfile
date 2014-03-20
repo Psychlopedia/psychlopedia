dad7fd2019485a92d5a3b2e8d0437be111365cef
Frank Ashurst <airongstring@gmail.com>
2014-03-19 22:01:00 -0300

    Updates DB to Postgres.

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '4.0.3'
-gem 'sqlite3'
+gem 'pg'
 gem 'thin'
 
 gem 'sass-rails', '~> 4.0.0'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140317031451) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "experiences", force: true do |t|
     t.string   "pseudonym"
     t.string   "title"
