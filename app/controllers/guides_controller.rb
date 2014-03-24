024785e87c4650ba580096cb3bddfa688ac1b019
Frank Ashurst <airongstring@gmail.com>
2014-03-24 14:29:34 -0300

    Adds the Guides skeleton.

---
+++ app/controllers/guides_controller.rb
@@ -0,0 +1,8 @@
+class GuidesController < ApplicationController
+  def index; end
+
+  def dxm; end
+  def lsd; end
+  def mdma; end
+  def nbomes; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Psychlopedia::Application.routes.draw do
+  # guides
+  get 'guides', to: 'guides#index', path: 'guias'
+  get 'dxm_guide', to: 'guides#dxm', path: 'guias/dxm'
+  get 'lsd_guide',  to: 'guides#lsd', path: 'guias/lsd'
+  get 'mdma_guide', to: 'guides#mdma', path: 'guias/mdma'
+  get 'nbomes_guide', to: 'guides#nbomes', path: 'guias/nbomes'
+
+  # experiences
   scope(path_names: {new: 'nueva'}) do
     resources :experiences, path: 'experiencias'
   end
