bfd778ce6aa44d26c04fb774f7796fdb0f0be363
Frank Ashurst <airongstring@gmail.com>
2014-03-26 03:15:38 -0300

    Updates Guides section: styles and the new Set & Setting sub-section.

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,6 +1,7 @@
 class GuidesController < ApplicationController
   def index; end
 
+  def set_and_setting; end
   def dxm; end
   def lsd; end
   def mdma; end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Psychlopedia::Application.routes.draw do
   # guides
   get 'guides', to: 'guides#index', path: 'guias'
+  get 'set_and_setting', to: 'guides#set_and_setting', path: 'guias/set_and_setting'
   get 'dxm_guide', to: 'guides#dxm', path: 'guias/dxm'
   get 'lsd_guide',  to: 'guides#lsd', path: 'guias/lsd'
   get 'mdma_guide', to: 'guides#mdma', path: 'guias/mdma'
