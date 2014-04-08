4367d4b8dfb7ba441d41c83022835266b7e9fdf1
Frank Ashurst <airongstring@gmail.com>
2014-04-08 20:19:50 -0300

    'Fixes' the Disqus issue.

--- app/assets/javascripts/experiences.js
+++ app/assets/javascripts/experiences.js
@@ -1,10 +1,5 @@
 (function($) {
   $(document).ready(function() {
-    $("a.toggle_comments").click(function(ev) {
-      ev.preventDefault();
-      $("#comments").toggle();
-    });
-
     $(".notice").on("click", function(ev) {
       $(ev.target).hide();
     });
