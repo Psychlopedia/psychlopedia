060e831ccfdebff362b09eedd426e7c4ffc53107
Frank Ashurst <airongstring@gmail.com>
2014-04-16 20:24:11 -0300

    Removes Guides entirely. The future is not the present.

--- app/controllers/guides_controller.rb
+++
@@ -1,9 +0,0 @@
-class GuidesController < ApplicationController
-  def index; end
-
-  def set_and_setting; end
-  def dxm; end
-  def lsd; end
-  def mdma; end
-  def nbomes; end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Psychlopedia::Application.routes.draw do
-  # guides
-  get 'guides', to: 'guides#index', path: 'guias'
-  get 'set_and_setting', to: 'guides#set_and_setting', path: 'guias/set_and_setting'
-  get 'dxm_guide', to: 'guides#dxm', path: 'guias/dxm'
-  get 'lsd_guide',  to: 'guides#lsd', path: 'guias/lsd'
-  get 'mdma_guide', to: 'guides#mdma', path: 'guias/mdma'
-  get 'nbomes_guide', to: 'guides#nbomes', path: 'guias/nbomes'
-
   # experiences
   scope(path_names: {new: 'nueva'}) do
     resources :experiences, path: 'experiencias'
