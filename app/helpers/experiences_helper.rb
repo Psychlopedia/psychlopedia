2bf44eba27366ff4c47dc635a117b5643fe9a502
Frank Ashurst <airongstring@gmail.com>
2014-04-22 19:54:21 -0300

    Moves titleize functionality to a helper. Yesh.

--- app/helpers/experiences_helper.rb
+++ app/helpers/experiences_helper.rb
@@ -3,4 +3,8 @@ module ExperiencesHelper
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, hard_wrap: true, filter_html: true, autolink: true, no_intraemphasis: true)
     markdown.render(text).html_safe
   end
+
+  def titleize(title)
+    title.split.map(&:capitalize).join(' ')
+  end
 end
