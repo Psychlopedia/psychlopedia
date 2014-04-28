3ed304047b3f5b2e62cb2860daea805af6e31147
Frank Ashurst <airongstring@gmail.com>
2014-04-28 15:27:19 -0300

    Setting testing password same as development password.

--- config/initializers/admin_password.rb
+++ config/initializers/admin_password.rb
@@ -1,6 +1,6 @@
 if ENV['PASSWORD'].present?
   ADMIN_PASSWORD = ENV['PASSWORD']
-elsif Rails.env.development?
+elsif Rails.env.development? || Rails.env.test?
   ADMIN_PASSWORD = 'password'
 else
   raise('Por favor especificá una contraseña de administración para Psychlopedia en la variable de entorno PASSWORD')
