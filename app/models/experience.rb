947ecd7e664ed8ca1870f5d631301f0c31c5e01c
Frank Ashurst <airongstring@gmail.com>
2014-04-28 10:38:12 -0300

    Removes old assumptions.

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -41,8 +41,6 @@ class Experience < ActiveRecord::Base
   end
 
   def defaults
-    self.pseudonym ||= 'anónimo'
-    self.title ||= 'sin título'
     self.license = 'CC BY 4.0' if self.is_licensed
     self.publication_date = Date.today
   end
