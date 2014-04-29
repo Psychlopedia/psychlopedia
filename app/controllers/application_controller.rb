36e4a192604dd9404136d350145fdc5ce02e751e
Frank Ashurst <airongstring@gmail.com>
2014-04-29 14:20:54 -0300

    [partial] Adds basic changes to internationalize Psychlopedia to English and Argentinian Spanish.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
   helper_method :logged_in?
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    set_correct_gotcha_implementation
+  end
+
+  def set_correct_gotcha_implementation
+    Gotcha.unregister_all_types
+    if I18n.locale == :en
+      Gotcha.register_type BackwardGotcha
+    else
+      Gotcha.register_type SpanishBackwardGotcha
+    end
+  end
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }
+  end
 
   private
 
--- config/initializers/gotcha.rb
+++ config/initializers/gotcha.rb
@@ -1,2 +1,3 @@
+# sensible defaults to es-AR
 Gotcha.unregister_all_types
 Gotcha.register_type SpanishBackwardGotcha
