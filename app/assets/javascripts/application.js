13b2dae3d236b94841a66aa2c7a4b24db09cc8f7
Frank Ashurst <airongstring@gmail.com>
2014-05-14 03:03:17 -0300

    Turbolinks' second chance.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery.turbolinks
 //= require jquery-ui.min
 //= require jquery_ujs
+//= require turbolinks
 //= require_tree .
